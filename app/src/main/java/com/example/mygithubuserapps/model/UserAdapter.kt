158f280cc25e60e19dd4e778efb6b5dad9bdd649
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-03-24 11:44:57 +0700

    new commit

--- app/src/main/java/com/example/mygithubuserapps/model/UserAdapter.kt
+++ app/src/main/java/com/example/mygithubuserapps/model/UserAdapter.kt
@@ -1,4 +1,27 @@
 package com.example.mygithubuserapps.model
 
-class UserAdapter {
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+
+class UserAdapter(private val listUser: ArrayList<UserModel>) : RecyclerView.Adapter<UserAdapter.UserViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.UserViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: UserAdapter.UserViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
 }
