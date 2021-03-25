dd86db7227f949efb47819e4a10eff3fec656485
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-03-25 11:02:58 +0700

    new commit

---
+++ app/src/main/java/com/example/mygithubuserapps/model/UserAdapterHorizontal.kt
@@ -0,0 +1,26 @@
+package com.example.mygithubuserapps.model
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class UserAdapterHorizontal : RecyclerView.Adapter<UserAdapterHorizontal.UserAdapterHorizontalViewHolder>() {
+
+
+    inner class UserAdapterHorizontalViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapterHorizontalViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: UserAdapterHorizontalViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
