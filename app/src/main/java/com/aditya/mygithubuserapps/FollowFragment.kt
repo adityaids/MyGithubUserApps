2801c6bfd31e0835ab6de8173298ab59cd5d0eae
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-04-09 18:19:15 +0700

    new commit

--- app/src/main/java/com/aditya/mygithubuserapps/FollowFragment.kt
+++ app/src/main/java/com/aditya/mygithubuserapps/FollowFragment.kt
@@ -1,6 +1,7 @@
 package com.aditya.mygithubuserapps
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -44,8 +45,12 @@ class FollowFragment : Fragment() {
         binding.rvFollow.layoutManager = LinearLayoutManager(view.context)
         binding.rvFollow.setHasFixedSize(true)
         binding.rvFollow.adapter = userAdapter
-        if (userList != null) {
-            userAdapter.setData(userList)
+        if (userList?.size != 0) {
+            if (userList != null) {
+                userAdapter.setData(userList)
+                Log.d("followFragment", userList.get(0).toString())
+                binding.loadingFollower.visibility = View.GONE
+            }
         }
     }
 
--- app/src/main/java/com/aditya/mygithubuserapps/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/aditya/mygithubuserapps/viewmodel/ProfileViewModel.kt
@@ -15,8 +15,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ProfileViewModel : ViewModel() {
     companion object{
-        const val urlUserFollower: String = "https://api.github.com/users/{username}/followers"
-        const val urlUserFollowing: String = "https://api.github.com/users/{username}/following"
+        const val urlUserFollower: String = "https://api.github.com/users/{username}/followers/"
+        const val urlUserFollowing: String = "https://api.github.com/users/{username}/following/"
     }
 
     private val listFollower = MutableLiveData<ArrayList<ApiUserModel>>()
